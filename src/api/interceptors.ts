81a80104ae3f0fddf1ce11a288d99b843905a755
Breno Rodrigues Dos Santos <brenobr10@hotmail.com>
2026-07-08 19:31:58 -0300

    ajusta interceptors com token

--- src/api/interceptors.ts
+++ src/api/interceptors.ts
@@ -1,12 +1,41 @@
 import type { AxiosInstance } from "axios";
 
-export function setupInterceptors(api: AxiosInstance) {
-  api.interceptors.request.use((config) => {
-    return config;
-  });
+import tokenStorage from "@/features/auth/storage/tokenStorage";
 
-  api.interceptors.response.use(
-    (response) => response,
-    (error) => Promise.reject(error)
-  );
+export function setupInterceptors(api: AxiosInstance) {
+
+    api.interceptors.request.use((config) => {
+
+        const token = tokenStorage.get();
+
+        if (token) {
+
+            config.headers.Authorization =
+                `Bearer ${token}`;
+
+        }
+
+        return config;
+    });
+
+    api.interceptors.response.use(
+
+        response => response,
+
+        error => {
+
+            if (error.response?.status === 401) {
+
+            tokenStorage.remove();
+
+            window.location.href = "/";
+
+        }
+
+        return Promise.reject(error);
+
+        }
+
+    );
+
 }
