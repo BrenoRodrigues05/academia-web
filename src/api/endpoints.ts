7e217b554a605ac9e8f65fbc17d3e393954f24dc
Breno Rodrigues Dos Santos <brenobr10@hotmail.com>
2026-07-08 18:46:42 -0300

    configura Axios e api

---
+++ src/api/endpoints.ts
@@ -0,0 +1,17 @@
+export const ENDPOINTS = {
+  AUTH: "/auth",
+
+  ALUNOS: "/api/alunos",
+
+  PERSONAIS: "/api/personais",
+
+  PLANOS: "/api/planos",
+
+  MATRICULAS: "/api/matriculas",
+
+  TREINOS: "/api/treinos",
+
+  EXERCICIOS: "/api/exercicios",
+
+  ITENS_TREINO: "/api/itens-treino",
+};
