74d3e13710c27897f7ce44880a1cc52505805fef
Breno Rodrigues Dos Santos <brenobr10@hotmail.com>
2026-07-21 19:33:29 -0300

    cria service para matriculas

---
+++ src/features/matriculas/api/MatriculaService.ts
@@ -0,0 +1,37 @@
+import BaseCrudService from "@/api/BaseCrudService";
+import api from "@/api/axios";
+import AlunoService from "@/features/alunos/services/AlunoService";
+import PlanoService from "@/features/planos/api/PlanoService";
+import type {Matricula} from "../types";
+
+    class MatriculaService
+    extends BaseCrudService<Matricula>{
+
+        constructor(){
+            super("/matriculas");
+        }
+
+    async getAlunos(){
+    return AlunoService.findAll();
+    }
+
+    async getPlanos(){
+    return PlanoService.findAll();
+    }
+
+    async findByAluno(id:number){
+    const response= await api.get(`${this.endpoint}/aluno/${id}`);
+    return response.data;
+    }
+
+    async findByPlano(id:number){
+    const response= await api.get(`${this.endpoint}/plano/${id}`);
+    return response.data;
+    }
+
+    async findAtivas(){
+    const response= await api.get(`${this.endpoint}/ativas`);
+    return response.data;
+}
+}
+export default new MatriculaService();
