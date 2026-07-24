9e3a388617426adb7a631b4f0091777b0f659598
Breno Rodrigues Dos Santos <brenobr10@hotmail.com>
2026-07-23 22:12:19 -0300

    protege rotas

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -10,39 +10,84 @@ import PersonaisPage from "@/features/personais/pages/PersonaisPage";
 import PlanosPage from "@/features/planos/pages/PlanosPage";
 import MatriculasPage from "@/features/matriculas/pages/MatriculasPage";
 import ExerciciosPage from "@/features/exercicios/pages/ExerciciosPage";
+import TreinosPage from "@/features/treinos/pages/TreinosPage";
 
 export default function AppRoutes() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={
-          <PublicRoute>
-          <LoginPage />
-          </PublicRoute>} />
-      <Route
-    path="/dashboard"
-    element={
-        <ProtectedRoute>
-            <DashboardPage />
-        </ProtectedRoute>
-    }
-/>
-        <Route path="/alunos" element={<AlunosPage />} />
-        <Route path="*" element={<NotFoundPage />} />
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <PublicRoute>
+                            <LoginPage />
+                        </PublicRoute>
+                    }
+                />
+                <Route
+                    path="/dashboard"
+                    element={
+                        <ProtectedRoute>
+                            <DashboardPage />
+                        </ProtectedRoute>
+                    }
+                />
 
-      <Route path="/personais" element={<PersonaisPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-    
-      <Route path="/planos" element={<PlanosPage />} />
-        <Route path="*" element={<NotFoundPage />} />
+                <Route
+                    path="/alunos"
+                    element={
+                        <ProtectedRoute>
+                            <AlunosPage />
+                        </ProtectedRoute>
+                    }
+                />
 
-        <Route path="/matriculas" element={<MatriculasPage />} />
-        <Route path="*" element={<NotFoundPage />} />
+                <Route
+                    path="/personais"
+                    element={
+                        <ProtectedRoute>
+                            <PersonaisPage />
+                        </ProtectedRoute>
+                    }
+                />
 
-        <Route path="/exercicios" element={<ExerciciosPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-    </BrowserRouter>
-    
-  );
+                <Route
+                    path="/planos"
+                    element={
+                        <ProtectedRoute>
+                            <PlanosPage />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route
+                    path="/matriculas"
+                    element={
+                        <ProtectedRoute>
+                            <MatriculasPage />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route
+                    path="/exercicios"
+                    element={
+                        <ProtectedRoute>
+                            <ExerciciosPage />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route
+                    path="/treinos"
+                    element={
+                        <ProtectedRoute>
+                            <TreinosPage />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
