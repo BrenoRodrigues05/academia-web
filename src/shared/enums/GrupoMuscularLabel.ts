8edb321ca45369a391cad0e033b825f50b8d5563
Breno Rodrigues Dos Santos <brenobr10@hotmail.com>
2026-07-25 14:13:22 -0300

    cria label para Enum de grupo muscular

---
+++ src/shared/enums/GrupoMuscularLabel.ts
@@ -0,0 +1,16 @@
+import { GrupoMuscular } from "./GrupoMuscular";
+
+export const GrupoMuscularLabel: Record<GrupoMuscular, string> = {
+    [GrupoMuscular.PEITORAL]: "Peitoral",
+    [GrupoMuscular.COSTAS]: "Costas",
+    [GrupoMuscular.OMBROS]: "Ombros",
+    [GrupoMuscular.BICEPS]: "Bíceps",
+    [GrupoMuscular.TRICEPS]: "Tríceps",
+    [GrupoMuscular.ANTEBRACO]: "Antebraço",
+    [GrupoMuscular.ABDOMEN]: "Abdômen",
+    [GrupoMuscular.GLUTEOS]: "Glúteos",
+    [GrupoMuscular.QUADRICEPS]: "Quadríceps",
+    [GrupoMuscular.POSTERIOR]: "Posterior",
+    [GrupoMuscular.PANTURRILHAS]: "Panturrilhas",
+    [GrupoMuscular.CARDIO]: "Cardio",
+};
