0b0a1d38c338960fde8231995f58e4dcdacc0691
Breno Rodrigues Dos Santos <brenobr10@hotmail.com>
2026-07-30 19:10:33 -0300

    ajuste rotas protegidas para verificar roles

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,18 +1,30 @@
 import { Navigate } from "react-router-dom";
 
 import { useAuth } from "@/features/auth/hooks/useAuth";
+import { UserRole } from "@/shared/enums/UserRole";
 
 type Props = {
-  children: React.ReactNode;
+    children: React.ReactNode;
+    roles?: UserRole[];
 };
 
-export default function ProtectedRoute({ children }: Props) {
+export default function ProtectedRoute({
+    children,
+    roles,
+}: Props) {
 
-  const { token } = useAuth();
+    const { token, user } = useAuth();
 
-  if (!token) {
-    return <Navigate to="/" replace />;
-  }
+    if (!token) {
+        return <Navigate to="/" replace />;
+    }
 
-  return children;
+    if (
+        roles &&
+        (!user || !roles.includes(user.role))
+    ) {
+        return <Navigate to="/dashboard" replace />;
+    }
+
+    return children;
 }
